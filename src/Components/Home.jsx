7bff38bd6cdab691365fafa23a1547c467f77c65
QadirzadeGultac <gultac.qadirzade@mail.ru>
2024-12-16 17:09:54 +0400

    refresh one

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import { HashRouter as Routes, Route, Link, NavLink } from 'react-router-dom';
+import './App.css';
+import Watch from './Components/Watch';
+import Stopwatch from './Components/Stopwatch';
+import Timer from './Components/Timer';
+import Home from './Components/Home';
 const Home = () => {
   return (
     <div>
-      
+       <div className='menu'>
+        <NavLink className='menus' to='/watch'>Saat</NavLink>
+        <NavLink className='menus' to='/stopwatch'>Saniyəölçən</NavLink>
+        <NavLink className='menus' to='/timer'>Taymer</NavLink>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/watch' element={<Watch />} />
+        <Route path='/stopwatch' element={<Stopwatch />} />
+        <Route path='/timer' element={<Timer />} />
+      </Routes>
     </div>
   )
 }
