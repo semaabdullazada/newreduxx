b22024e9a8add645d33f471ab5a0e4551dc4f95b
QadirzadeGultac <gultac.qadirzade@mail.ru>
2024-12-16 17:04:18 +0400

    refresh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Link, NavLink, Routes } from 'react-router-dom';
+import { HashRouter as Routes, Route, Link, NavLink } from 'react-router-dom';
 import './App.css';
 import Watch from './Components/Watch';
 import Stopwatch from './Components/Stopwatch';
@@ -7,18 +7,18 @@ import Home from './Components/Home';
 function App() {
   return (
     <div className="App">
-       <div className='menu'>
-       <NavLink className='menus' to='/watch'>Saat</NavLink>
-       <NavLink className='menus' to='/stopwatch'>Saniyəölçən</NavLink>
-       <NavLink className='menus' to='/timer'>Taymer</NavLink>
- </div>
- <Routes>
-      <Route path='/' element={<Home />} />
-         <Route path='/watch' element={<Watch />} />
-         <Route path='/stopwatch' element={<Stopwatch />} />
-         <Route path='/timer' element={<Timer />} />
-    </Routes>
-     </div>
+      <div className='menu'>
+        <NavLink className='menus' to='/watch'>Saat</NavLink>
+        <NavLink className='menus' to='/stopwatch'>Saniyəölçən</NavLink>
+        <NavLink className='menus' to='/timer'>Taymer</NavLink>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/watch' element={<Watch />} />
+        <Route path='/stopwatch' element={<Stopwatch />} />
+        <Route path='/timer' element={<Timer />} />
+      </Routes>
+    </div>
   );
 }
 
