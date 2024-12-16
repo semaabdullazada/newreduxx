5cec3993dc80166f8ac85e4cbfe80a1cea7b04cb
QadirzadeGultac <gultac.qadirzade@mail.ru>
2024-12-16 17:16:51 +0400

    refresh tree

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,25 +1,18 @@
-import React from 'react'
-import { HashRouter as Routes, Route, Link, NavLink } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Home.css';
-import Watch from './Watch';
-import Stopwatch from './Stopwatch';
-import Timer from './Timer';
+
 const Home = () => {
   return (
     <div>
-       <div className='menu'>
+      <h1>Əsas Səhifə</h1>
+      <div className='menu'>
         <NavLink className='menus' to='/watch'>Saat</NavLink>
         <NavLink className='menus' to='/stopwatch'>Saniyəölçən</NavLink>
         <NavLink className='menus' to='/timer'>Taymer</NavLink>
       </div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/watch' element={<Watch />} />
-        <Route path='/stopwatch' element={<Stopwatch />} />
-        <Route path='/timer' element={<Timer />} />
-      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
