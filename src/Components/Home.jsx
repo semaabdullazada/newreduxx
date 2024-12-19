16f91479f27bc5e9e9ee4bef238617c61f5cfe37
QadirzadeGultac <gultac.qadirzade@mail.ru>
2024-12-19 09:29:19 +0400

    deploy

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,12 +5,6 @@ import './Home.css';
 const Home = () => {
   return (
     <div>
-      <h1>Əsas Səhifə</h1>
-      <div className='menu'>
-        <NavLink className='menus' to='/watch'>Saat</NavLink>
-        <NavLink className='menus' to='/stopwatch'>Saniyəölçən</NavLink>
-        <NavLink className='menus' to='/timer'>Taymer</NavLink>
-      </div>
     </div>
   );
 };
